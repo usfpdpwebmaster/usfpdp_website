4da9d952d3cf23514fb61f91f1f39722d4c703da
Thom <s.thomvphorn@gmail.com>
2025-01-10 21:51:35 -0500

    4 sure works

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/usfpdp_website/', // Replace with your repository name if deploying to GitHub Pages
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-modal'],
-    },
-  },
 });
