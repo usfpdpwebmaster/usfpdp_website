6efd8127709bf382a47236c7dea522d168eeeee4
Thom <s.thomvphorn@gmail.com>
2025-01-10 21:39:36 -0500

    update all files to original

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/usfpdp_website/', // Replace with your repository name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
