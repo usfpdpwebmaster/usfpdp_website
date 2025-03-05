e9abf9e324bf1c18d5bb5bb4e1c6989888646b7c
thomveebol <s.thomvphorn@gmail.com>
2025-03-05 13:22:15 -0500

    volleyball on left

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -76,7 +76,7 @@ const Home = () => {
   {/* Change links here for maintenance throughout the semester for LATEST UPDATES*/}
   const instagramPosts = [
     {
-      url: 'https://www.instagram.com/p/DGQ60q5xT9J/',
+      url: 'https://www.instagram.com/p/DGeCGOByb9U/',
       embed: true,
     },
     {
