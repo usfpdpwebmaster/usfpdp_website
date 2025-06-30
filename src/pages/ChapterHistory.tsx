2a69ef65401cba3e60249b4bbd99ab035270c762
Phu <trangiaphu47@gmail.com>
2025-06-30 17:48:15 -0400

    Update Chapter History

--- src/pages/ChapterHistory.tsx
+++ src/pages/ChapterHistory.tsx
@@ -17,22 +17,23 @@ const ChapterHistory = () => {
             <img src="/alphas_backshots.jpg" className="w-full md:w-full rounded-lg shadow-md mb-8 md:mb-0" />
           </div>
           <p className="leading-relaxed">
-            In 2008, ten men who were already <span className="text-red-500">leaders</span> in their own rights sought to bring a greater
-            presence to the <span className="text-red-500">Asian voice</span> at the University of South 
-            Florida at the Tampa Bay Area. 
+            In 2008, ten men who were already <span className="text-red-500">leaders</span> in their own rights came together with a shared vision:
+            to amplify the <span className="text-red-500">Asian voice</span> at the University of South 
+            Florida and the Tampa Bay Area. Recognizing a lack of culture representation and support, they envisioned a brotherhood that would not only foster 
+            unity and pride but also challenging prevailing stereotypes and provide a lasting platform for leadership and growth.  
           </p>
           <p className="leading-relaxed">
             On <span className="text-red-500">November 9th</span>, <span className="text-red-500">2008</span>, these ten men <span className="text-red-500">emerged</span> as <span className="text-red-500">Pi Delta Psi Fraternity, Inc.</span> at the 
             University of South Florida. Guided by the four pillars of <span className="text-red-500">Academic Achievement</span>, <span className="text-red-500">Cultural
-            Awareness</span>, <span className="text-red-500">Righteousness</span>, and <span className="text-red-500">Friendship/Loyalty</span>. This eternal <span className="text-red-500">brotherhood</span> continues to <span className="text-red-500">empower </span>
-            its members to break stereotypes, fight discrimination, celebrate diversity, and set
-            new standards for academic excellence.
+            Awareness</span>, <span className="text-red-500">Righteousness</span>, and <span className="text-red-500">Friendship/Loyalty</span>. From its earliest days, This <span className="text-red-500">brotherhood</span> continues to <span className="text-red-500">empower </span>
+            its members to rise above expectations, break stereotypes, fight discrimination, celebrate their heritage, and set
+            higher for standards themselves and their communities.
             
           </p>
           <p className="leading-relaxed">
-            Since its inception, <span className="text-red-500">Pi Delta Psi</span> at the University of South Florida aims to be steadfast
-            in adhering to its morals and educating its members and the student body to help <span className="text-red-500">maximize </span>
-             the potential of a South Florida Bull!
+            From its founding to the present day, <span className="text-red-500">Pi Delta Psi</span> at the University of South Florida remains committed
+            to its core values and dedicated to empowering its members and the student body to help <span className="text-red-500">maximize </span>
+             the potential of a South Florida Bull and leave their mark on the world!
           </p>
         </div>
       </div>
